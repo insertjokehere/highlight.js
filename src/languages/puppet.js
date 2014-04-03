1d3bda6a0f128d6a777803417b86fcc7ed741058
William Hughes <williamh@catalyst.net.nz>
2014-04-04 11:30:46 +1300

    Further work improving parsing

--- src/languages/puppet.js
+++ src/languages/puppet.js
@@ -1,22 +1,74 @@
 /*
 Language: Puppet
 */
-//Based off http://downloads.puppetlabs.com/puppet/puppet.vim
 function (hljs) {
-    var DEFINE = {
-	beginKeywords:'class define node',
-	end: '{',
-	className: 'function'
+    var VAR = {
+	begin: /$\w+/
     }
+    var VAR_REPLACE = {
+	begin: /\$\{\w+\}/,
+	relevance: 10,
+	className: 'variable'
+    }
+
+    var STRING = {
+	className: 'string',
+	variants: [
+	    {
+		begin: /"/,
+		end: /"/,
+		contains: [VAR_REPLACE],
+	    },
+	    hljs.APOS_STRING_MODE
+	]
+    }
+
+    var REFERENCE = {
+	begin: /[A-Z][a-z_]+(::|[A-Z][a-z_]+)*\s*\[/,
+	end: /\]/,
+	contains: [STRING],
+	className: 'reference'
+    }
+
+    var RESOURCE_PARAM = {
+	className: 'param',
+	begin: /[a-z_\-]+\s*[=+]\>[a-z_\-]+/
+    }
+
+    var RESOURCE = {
+	className: 'resource',
+	variants: [
+	    {
+		begin: /[a-z_]+(::|[a-z_]+)*\s*\{/,
+		end: /\}/,
+		contains: [STRING, RESOURCE_PARAM]
+	    },
+	    REFERENCE
+	],
+    }
+
+    var PARAM_LIST = {
+	begin: /\(/,
+	end: /\)/,
+	contains: [VAR],
+	className: 'params'
+    }
+
+    var DEFINE = {
+	beginKeywords: 'class define',
+	className: 'class',
+	contains: [PARAM_LIST]
+    }
+
     return {
 	case_insensitive: false,
 	
 	aliases: ['pp'],
 	keywords: {
-	    keyword: 'and default in import or unless',
+	    keyword: 'and default in import or unless present absent node if',
 	    literal: 'false true undef',
 	},
 	
-	contains: [hljs.HASH_COMMENT_MODE, DEFINE]
+	contains: [hljs.HASH_COMMENT_MODE, STRING, VAR, RESOURCE, DEFINE]
     }
 }
